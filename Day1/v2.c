15ea9296064b1a48a4cd19803fa1579848c726ed
mdsiaofficial <mdsiaofficial@gmail.com>
2024-05-27 09:34:43 +0600

    first commit

---
+++ Day1/v2.c
@@ -0,0 +1,12 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+#include <time.h>
+#include <math.h>
+
+int main(){
+
+  printf("Preprocessor used to run.");
+  
+}
