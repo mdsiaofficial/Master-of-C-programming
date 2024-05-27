ae7c37e12b128268e05435c60059e98191613721
mdsiaofficial <mdsiaofficial@gmail.com>
2024-05-27 09:36:19 +0600

    comment added

--- Day1/v2.c
+++ Day1/v2.c
@@ -4,9 +4,12 @@
 #include <conio.h>
 #include <time.h>
 #include <math.h>
+// these all are header files
+
 
 int main(){
 
   printf("Preprocessor used to run.");
-  
+  // fuction to print. this function came from <stdio.h>
+   
 }
