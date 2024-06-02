41f617b2425c730c3820add3f3e1d8222c5ba874
mdsiaofficial <mdsiaofficial@gmail.com>
2024-06-02 18:54:58 +0600

    "Modified v5.c to change calculation of 'result' and added additional printf statement; updated v5.exe accordingly."

--- Day2/v5.c
+++ Day2/v5.c
@@ -3,6 +3,7 @@ int main(){
   int two = 2, three = 3, eight = 8;
   printf("The value of two is %d\n", two);
 
-  int result = (two + three) * eight / three;
+  int result = ((two++) + (three++)) * (eight--) / (three--);
   printf("The result is here when this is a new program with multiple lines of string to print\n the main result is here... %d\n", result);
+  printf("The value of two is %d\n", two);
 }
