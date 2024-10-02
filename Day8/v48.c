b9a4a6eaa31e26fd786b91ba8bd62ac439cbce35
mdsiaofficial <mdsiaofficial@gmail.com>
2024-10-02 11:41:10 +0600

    "Added pyramid pattern printing code and commented out reverse pyramid pattern printing code."

--- Day8/v48.c
+++ Day8/v48.c
@@ -2,11 +2,33 @@
 int main()
 {
   int n=10;
+
+  // pyramid
   for(int i=1; i<=n; i++){
-    for(int j=1; j<=n; j++){
-      printf("*");
+    for(int j=1; j<=2*n-1; j++){
+      if( j >= n-(i-1) && j<= n+(i-1)){
+        printf("*");
+      }
+      else{
+        printf(" ");
+      }
     }
     printf("\n");
   }
+  printf("\n");
+
+  // // reverse pyramid
+  // for(int i=1; i<=n; i++){
+  //   for(int j=1; j<=2*n-1; j++){
+  //     if( i >= i && i<= 2*n-1-i){
+  //       printf("*");
+  //     }
+  //     else{
+  //       printf(" ");
+  //     }
+  //   }
+  //   printf("\n");
+  // }
+
   return 0;
 }
