2eb1a21a59cb20556b14471c0d64c7cec8218c2a
mdsiaofficial <mdsiaofficial@gmail.com>
2024-10-09 09:11:54 +0600

    Added a space after `for` statement in `checkPrime` function.

--- Day9/v52.c
+++ Day9/v52.c
@@ -5,7 +5,7 @@
 bool checkPrime (int n){
   if(n <= 1) return false;
   else if(n == 2 || n == 3) return true;
-  for(int i=2; i <= sqrt(n); i++){
+  for(int i=2; i <= sqrt(n); i++) {
     if(n % i == 0) return false;
   }
   return true;
